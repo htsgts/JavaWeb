01909fa72599a7255fc8f9c68ae119fc8b6086d7
htsgts <127921031+htsgts@users.noreply.github.com>
2025-11-10 17:20:49 +0900

    spring 실습

---
+++ spring/day04_src/main/java/com/koreait/board/bean/vo/ClassRoom.java
@@ -0,0 +1,32 @@
+package com.koreait.board.bean.vo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.koreait.board.util.MyUtil;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Component  // 나 좀 컨테이너에서 관리해줘
+@Slf4j
+@Data
+public class ClassRoom {
+	private int no;
+	
+	@Autowired
+	private Desk desk1;
+	
+	@Autowired
+	private Desk desk2;
+	
+	@Autowired
+	private Chair chair1;
+	
+	@Autowired
+	private Chair chair2;
+	
+	public ClassRoom() {
+		log.info(MyUtil.BLUE + "New Classroom Open!!" + MyUtil.END);
+	}
+}
