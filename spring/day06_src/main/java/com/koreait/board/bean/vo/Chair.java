cada937e8aa7706fddda3288f189d71813d424e1
htsgts <127921031+htsgts@users.noreply.github.com>
2025-11-17 10:59:23 +0900

    spring 실습

---
+++ spring/day06_src/main/java/com/koreait/board/bean/vo/Chair.java
@@ -0,0 +1,17 @@
+package com.koreait.board.bean.vo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class Chair {
+	private String name;
+	private int numOfLegs;
+	
+	public Chair() {
+		this.name = "Tom Sawyer";
+		this.numOfLegs = 4;
+	}
+}
---
+++ spring/day06_src/test/java/com/koreait/board/mapper/BoardMapperTest.java
@@ -0,0 +1,51 @@
+package com.koreait.board.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.koreait.board.bean.vo.BoardVO;
+import com.koreait.board.bean.vo.TestVO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+public class BoardMapperTest {
+
+	@Autowired
+	private BoardMapper mapper;
+	
+	//@Test
+	public void insertTest() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("나도 축구 봄 눈 썩음");
+		vo.setContent("경기 내용이 수준 이하");
+		vo.setWriter("경기본자");
+		
+		mapper.insert(vo);
+		log.info("BoardVO : " + vo);
+		
+		TestVO vo2 = new TestVO();
+		mapper.insert2(vo2);
+		
+		mapper.insertSelectKey(vo);
+		log.info("BoardVO : " + vo);
+		log.info("BoardVO : " + vo.getRealBno());
+		
+		// 임의의 글 50개 인서트를 해보세요.
+		for(int i=0; i<50; i++) {
+			BoardVO board = new BoardVO();
+			board.setTitle("제목" + i);
+			board.setContent("내용" + i);
+			board.setWriter("글쓴이" + i);
+			mapper.insertSelectKey(board);
+		}
+	}
+	
+	@Test
+	public void testGetList() {
+		mapper.getList()
+		      .forEach(board->log.info("Board : " + board));
+	}
+}
