75b315dd5302d99888d59a5da45b8229dc7f2839
htsgts <127921031+htsgts@users.noreply.github.com>
2025-12-07 16:34:45 +0900

    spring 실습

---
+++ spring/day11_src/main/java/com/koreait/board/bean/vo/BoardVO.java
@@ -0,0 +1,25 @@
+package com.koreait.board.bean.vo;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+public class BoardVO {
+	private Long bno;
+	private String title;
+	private String content;
+	private String writer;
+	private String regdate;
+	private String updatedate;
+	private AttachFileVO[] attachFile;
+	
+//	public Long getBno() {
+//		log.info("getBno() Called");
+//		return 10001L;
+//	}
+	
+	public Long getRealBno() {
+		return bno;
+	}
+}
